d58403c4357fa4d8e82fe8fd089beda20d7f05e4
alishasaxena45 <alishasaxena324@gmail.com>
2026-07-12 20:51:26 +0530

    Create script_2.js

---
+++ 25ESKCX007/Day 7/script_2.js
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+<script>
+    let age=25;
+    console.log(age);
+    if (age>18){
+        console.log("eligible");
+    }
+    else{
+        console.log("not eligible")
+    }
+
+    let name="poorwansh";
+    console.log(name);
+
+    let a=10;
+    a=30;
+    console.log(a);
+
+    let number;
+    console.log(number);
+
+    let marks=13426374894007866898567798985734565896645468790998454535679086545568900098765434556789876543557834545345409009645689909989765487900987654569008765448976545678553214567898776454356689808975643454887654354;
+    console.log(marks);
+
+    marks=false;
+    console.log(typeof(marks));
+
+</script>
+</html>
