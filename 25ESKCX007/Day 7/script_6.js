6cfd3e152d8aa0832251d0a892653dc57ceeccb1
alishasaxena45 <alishasaxena324@gmail.com>
2026-07-12 21:00:24 +0530

    Create script_6.js with HTML and array examples
    Added HTML structure and JavaScript code to demonstrate array usage.

---
+++ 25ESKCX007/Day 7/script_6.js
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+<script>
+//     let obj={
+//         name: "poorwansh",
+//         age: "20",
+//  };
+
+
+// array consstructor
+let brr= new Array("love", 1, true);
+brr.push("hello");
+console.log(brr);
+
+let arr=[10,20,30,40];
+arr.forEach((value, index) => {
+    console.log("number:" , value, "index", index);
+})
+
+
+</script>
+</html>
